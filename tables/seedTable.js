f40f3848819642ad7a555907753fcb0d7d8d341a
Jeremie Inkura <jeremie.inkura@gmail.com>
2026-04-20 20:18:37 +0200

    Addition of the eslint config and execution of required fixes

--- tables/seedTable.js
+++ tables/seedTable.js
@@ -1,16 +1,16 @@
-import sqlite3 from "sqlite3";
-import { open } from "sqlite";
-import path from "node:path";
-import { pokemons } from "../data/data.js";
+import sqlite3 from 'sqlite3';
+import { open } from 'sqlite';
+import path from 'node:path';
+import { pokemons } from '../data/data.js';
 
 async function seedTable() {
   const db = await open({
-    filename: path.join(process.cwd(), "db.sqlite"),
+    filename: path.join(process.cwd(), 'db.sqlite'),
     driver: sqlite3.Database,
   });
 
   try {
-    await db.exec("BEGIN TRANSACTION");
+    await db.exec('BEGIN TRANSACTION');
 
     for (const { name, type, height, weight, colors, stage } of pokemons) {
       await db.run(
@@ -21,14 +21,14 @@ async function seedTable() {
       );
     }
 
-    await db.exec("COMMIT");
-    console.log("Data inserted successfully");
+    await db.exec('COMMIT');
+    console.log('Data inserted successfully');
   } catch (error) {
-    await db.exec("ROLLBACK");
-    console.error("Error inserting data:", error.message);
+    await db.exec('ROLLBACK');
+    console.error('Error inserting data:', error.message);
   } finally {
     await db.close();
-    console.log("Database connection closed");
+    console.log('Database connection closed');
   }
 }
 
