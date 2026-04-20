680223cc8d4fd636705665c3e40ad19fdec673e9
Jeremie Inkura <jeremie.inkura@gmail.com>
2026-04-20 20:26:34 +0200

    Re-executed the lint fixes

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-import express from "express";
-import { apiRouter } from "./routes/apiRoutes.js";
+import express from 'express';
+import { apiRouter } from './routes/apiRoutes.js';
 
 const PORT = 8000;
 
 const app = express();
 
-app.use("/api", apiRouter);
+app.use('/api', apiRouter);
 app.use((req, res) => {
   res.status(404).json({
-    error: "Welcome to the Pokémon API! Use /api to access the endpoints.",
+    error: 'Welcome to the Pokémon API! Use /api to access the endpoints.',
   });
 });
 
